44ce11c0f20874c39ab138e036dcf5cbb57ea77a
Brigitta Rucz <brigitta.rucz@yahoo.com>
2019-08-28 15:55:50 +0200

    styling

--- javascript.js
+++ javascript.js
@@ -1,15 +1,14 @@
-let baseLink = "http://petlatkea.dk/2019/students1991.json";
+let baseLink = 'http://petlatkea.dk/2019/students1991.json';
 let array1 = [];
 
 function loadList(link) {
-    fetch(link).then(e => e.json()).then(data => consoleLog(data));
+	fetch(link).then((e) => e.json()).then((data) => consoleLog(data));
 }
 
 function consoleLog(data) {
-    data.forEach(element => {
-        array1.push(element);
-        console.log(array1);
-    });
-};
+	data.forEach((element) => {
+		array1.push(element);
+	});
+}
 
 loadList(baseLink);
