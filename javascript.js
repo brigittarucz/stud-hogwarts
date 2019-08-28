87d6c465ff7271758a2f2cdaafacde04c241b2fc
Brigitta Rucz <brigitta.rucz@yahoo.com>
2019-08-28 16:55:22 +0200

    Trigger rebuild

--- javascript.js
+++ javascript.js
@@ -2,13 +2,23 @@ let baseLink = 'http://petlatkea.dk/2019/students1991.json';
 let array1 = [];
 
 function loadList(link) {
-	fetch(link).then((e) => e.json()).then((data) => consoleLog(data));
+    fetch(link).then((e) => e.json()).then((data) => consoleLog(data));
 }
 
 function consoleLog(data) {
-	data.forEach((element) => {
-		array1.push(element);
-	});
+    data.forEach((element) => {
+        array1.push(element);
+    });
+
+    let n = array1.length;
+
+    for (let i = 0; i < n; i++) {
+        let ul = document.getElementsByClassName(array1[i]["house"]);
+        let li = document.createElement("li");
+        li.textContent = array1[i]["fullname"];
+        ul[0].appendChild(li);
+    }
+
 }
 
 loadList(baseLink);
